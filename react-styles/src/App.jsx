8b74f26f66e067ec13b4aecf4d5790710d773a43
amirreza <notname1957@gmail.com>
2024-02-26 19:13:31 +0330

    added styles

---
+++ react-styles/src/App.jsx
@@ -0,0 +1,13 @@
+import EmotionButton from "./components/EmotionButton";
+import StyledComponentsButton from "./components/StyledComponentsButton";
+
+function App() {
+  return (
+    <>
+      <StyledComponentsButton />
+      <EmotionButton />
+    </>
+  );
+}
+
+export default App;
